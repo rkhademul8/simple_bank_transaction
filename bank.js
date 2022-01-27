8da118bce3f030ed69df5a3d6adf2fd70f45edd0
rkhademul8 <rkhademul8.com>
2022-01-27 21:58:31 +0600

    banking-layout_part_done

--- bank.js
+++ bank.js
@@ -1,11 +1,16 @@
 document.getElementById('login-submit').addEventListener('click',function(){
+    // get user email
     const emailField=document.getElementById('user-email')
     const userEmail=emailField.value
+    // get user password
     const passwordField=document.getElementById('user-password')
     const userPassword=passwordField.value
-
+    // check email and password
     if(userEmail=='khademul@gmail.com' && userPassword=='1234'){
        window.location.href='banking.html'
+    }
+    else{
+        alert('oh man !! check uername and password')
     }
 
 })
