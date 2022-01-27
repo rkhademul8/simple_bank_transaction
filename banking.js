773d45d38810f9864b97f4b0300c460f6abb8855
rkhademul8 <rkhademul8.com>
2022-01-28 01:18:02 +0600

    withdraw_part_done

--- banking.js
+++ banking.js
@@ -22,3 +22,28 @@ document.getElementById('deposite-button').addEventListener('click',function(){
      balanceTotal.innerText=tatoalBalance
     
 })
+
+// handle withdraw button event
+
+document.getElementById('withdraw-button').addEventListener('click',function(){
+    const withdrawInput=document.getElementById('withdraw-input')
+    const withdrawInputText=withdrawInput.value
+    withdrawInput.value=''
+    const withdrawAmount=parseFloat(withdrawInputText)
+
+    // update  withdraw total
+
+    const withdrawTotal=document.getElementById('withdraw-total')
+    const previouswithdrawText=withdrawTotal.innerText
+    const previouswithdrawAmount=parseFloat(previouswithdrawText)
+    const totalWithdraw=previouswithdrawAmount+withdrawAmount
+    withdrawTotal.innerText=totalWithdraw
+
+    // balance update after withdraw
+    const balanceTotal=document.getElementById('balance-input')
+    const previousBalanceText=balanceTotal.innerText
+    const previousBalanceAmount=parseFloat(previousBalanceText)
+    const tatoalBalance=previousBalanceAmount-withdrawAmount
+    balanceTotal.innerText=tatoalBalance
+
+})
